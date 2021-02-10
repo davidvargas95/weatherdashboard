9951ec58ea7da609999458d976c7967058d94540
David Vargas <davidvargas95@gmail.com>
2021-02-09 19:56:30 -0800

    search displays city name below input bar

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,11 +10,27 @@ function weather(city) {
         console.log(response);
     })
 
+    var temp = (response.main.temp);
+    var date = moment().format("MM/DD/YYYY");
+    var icon = (response.weather[0].icon);
+    // var URL = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+
+    $("#cityName").html("<h3>" + response.name + " " + date + "</h3>");
+    // $("weatherImage").attr("src", weatherURL);
+    $("#temp").text("Temperature: " + temp.toFixed(2) + "°F");
+    $("#rh").text("Humidity: " + response.main.humidity + "%");
+    $("wind").text("Wind: " + response.wind.speed + "mph");
+
+    $("selectedCity").removeClass("d-none");
+
 }
 
 $("#searchButton").on("click", function() {
 
     var city = $("#cityInput").val();   
+    var cityList = $(`<button class= "list-group-item mx-3 mr-sm-2 w-50" data-city= "${city}">${city}</button>`);
+
+    $("#cityList").prepend(cityList);
 
     weather(city);
 });
